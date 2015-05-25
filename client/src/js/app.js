747a8233d26aa69a56f2b3fd62ad069beaf469f5
liyuan-rey <liyuan-rey@users.noreply.github.com>
2015-05-25 16:35:16 +0800

    load jquery with requirejs, do some client code

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,1 +1,26 @@
 // app.js
+
+requirejs.config({
+    baseUrl: 'js',
+    shim: {
+
+    },
+    paths: {
+        jquery: 'vendor/jquery'
+    },
+    packages: [
+
+    ]
+});
+
+require(['require', 'jquery'], function(require, $) {
+    'use strict';
+
+    $('log').text('running in app.js\n');
+
+    require(['jquery', 'auth/login'], function($, login) {
+        $('testbtn').click = login.onTest;
+    });
+
+});
+
--- client/src/js/auth/login.js
+++ client/src/js/auth/login.js
@@ -1,12 +1,19 @@
 // login.js
 
-define([
-	'requirejs',
-	'jquery'
-], function(require, $) {
-	'use strict';
-	var user = $('username').value;
-	var pwd = $('password').value;
-	
-	
+define(['require', 'jquery'], function(require, $) {
+    'use strict';
+
+    var log = $('log');
+    log.textContent += 'loading login.js\n';
+
+    var obj = {};
+    obj.onTest = function (data, fn) {
+        var user = $('username').value;
+        var pwd = $('password').value;
+
+        var log = $('log');
+        log.textContent += user + ':' + pwd + '\n';
+    };
+
+    return obj;
 });
